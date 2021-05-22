7c0ae2a9d16b7bb9a991c10d9801c9a3ab14b610
Neezar <rforlyffe@gmaicl.com>
2021-05-22 23:31:45 +0800

    add chat module

--- client/next.config.js
+++ client/next.config.js
@@ -7,8 +7,8 @@ module.exports = {
       },
       // Rewrite everything else to use `pages/app`
       {
-        source: "/app/:path*",
-        destination: "/app",
+        source: "/:path*",
+        destination: "/",
       },
     ];
   },
