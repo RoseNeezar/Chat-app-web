4759d97f48821011b246b65006c55f4333882fa2
Neezar <rforlyffe@gmaicl.com>
2021-06-07 22:45:05 +0800

    add chat message

--- client/src/app/PageComponent/messenger/Messages/component/MessageContent.tsx
+++ client/src/app/PageComponent/messenger/Messages/component/MessageContent.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { FC } from "react";
 import { useSelector } from "react-redux";
 import { IState } from "../../../../redux/reducers/rootReducer";
+import { Channel } from "../../../../redux/types/chat.type";
 import MessagBubble from "./MessagBubble";
 
-const MessageContent = () => {
+interface IMessageContent {
+  channel: Channel;
+}
+
+const MessageContent: FC<IMessageContent> = ({ channel }) => {
   return (
-    <div className="flex space-x-2">
-      <MessagBubble />
+    <div className="flex flex-col max-w-max">
+      {channel.message.map((msg) => {
+        return <MessagBubble key={msg.id} message={msg} />;
+      })}
     </div>
   );
 };
--- client/src/app/PageComponent/messenger/Messages/component/MessageHeader.tsx
+++ client/src/app/PageComponent/messenger/Messages/component/MessageHeader.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { FC } from "react";
+import { Channel } from "../../../../redux/types/chat.type";
 
-const MessageHeader = () => {
-  return <div>Header</div>;
+interface IMessageHeader {
+  channel: Channel;
+}
+
+const MessageHeader: FC<IMessageHeader> = ({ channel }) => {
+  return (
+    <div className="flex flex-row">
+      {channel.chatUser.map((usr) => {
+        return (
+          <div key={usr.id} className="px-4 py-2 bg-dark-main rounded-2xl">
+            <p>{usr.user.username}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MessageHeader;
