789127cdd7beca411c753ca53c6ec0495b7e4e97
Neezar <rforlyffe@gmaicl.com>
2021-06-08 21:42:15 +0800

    fix protected route

--- client/src/app/utils/ProtectedRoute.tsx
+++ client/src/app/utils/ProtectedRoute.tsx
@@ -1,35 +1,15 @@
-import axios from "axios";
-import React, { FC, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { FC } from "react";
 import { Redirect, Route, RouteProps } from "react-router";
-import { ACurrentUser } from "../redux/actions/auth";
+import isAuth from "./isAuth";
 
 const ProtectedRoute: FC<RouteProps> = ({ children, ...rest }) => {
-  const [isLogged, setIsLogged] = useState(true);
-  const dispatch = useDispatch();
-
-  const isAuth = async () => {
-    const check = await axios
-      .get("/auth/me")
-      .then((user) => {
-        return true;
-      })
-      .catch((err) => {
-        return false;
-      });
-
-    setIsLogged(check);
-  };
-  useEffect(() => {
-    dispatch(ACurrentUser());
-    isAuth();
-  }, [isAuth, setIsLogged]);
+  const auth = isAuth();
 
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        isLogged ? (
+        auth.currentUser ? (
           children
         ) : (
           <Redirect
