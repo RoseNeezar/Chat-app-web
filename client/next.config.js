b6382d3df60e22127270b32c3a5cfacba1c1495b
Neezar <rforlyffe@gmaicl.com>
2021-06-10 12:16:44 +0800

    Add react native

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      "react-native$": "react-native-web",
+    };
+    config.resolve.extensions = [
+      ".web.js",
+      ".web.ts",
+      ".web.tsx",
+      ...config.resolve.extensions,
+    ];
+    return config;
+  },
   webpackDevMiddleware: (config) => {
     config.watchOptions = {
       poll: 1000,
